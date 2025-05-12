1fa7399d88f0cc2ec1f94cd7bdb22c610a00c745
Elena Margolin <emargolin@apple.com>
2025-05-11 23:30:42 -0700

    Adjusting styling of site

--- code.js
+++ code.js
@@ -1,8 +1,8 @@
+let skins = ["skins/skin-1.png", "skins/skin-2.png", "skins/skin-3.png", "skins/skin-4.png", "skins/skin-5.png"]
+let dresses = ['dresses/dress-1.png', 'dresses/dress-2.png', 'dresses/dress-3.png', 'dresses/dress-4.png']
+let hairs = [["hair/gingerhairfront.png", "hair/gingerhairback.png"], ["hair/hairfront-1.png", "hair/hairback-1.png"]]
 
-window.onload=init;
-
-function init()
-{
+window.onload = () => {
     console.log("window has loaded");
     state.i=1;
     state.j=1;
@@ -11,125 +11,46 @@ function init()
 }
 
 var state = {
-    i:0, // 0. nextDress> +1 , 1. nextDress> +1 , 0.
+    i:0,
     j:0,
     k:0,
     l:0
 };
 
-var dresses = ['dress-1.png', 'dress-2.png']
-
 function nextdress()
 {
-    console.log("inside function nextdress");
-    console.log(state.i);
-
     var dress = document.getElementById("clothes");
-
-    // state.i = (state.i + 1) % length(dresses)
-    // dress.src = dresses[state.i]
-
-    if(state.i===0){
-        dress.src =  'dress-1.png'
-        state.i++;
-        console.log(state.i);
-    }
-    else
-    if(state.i===1){
-        dress.src =  'dress-2.png'
-        state.i++;
-        console.log(state.i);
-    }
-    else
-    if(state.i===2){
-        dress.src =  'dress-3.png'
-        state.i++;
-        console.log(state.i);
-    }
-    else
-    if(state.i===3){
-        dress.src = 'dress-4.png'
-        state.i=0;
-        console.log(state.i);
-    } 
+    state.i = (state.i + 1) % dresses.length
+    dress.src = dresses[state.i];
 }
 
-// TODO: bug here. 
 function nextshirt()
 {
-    console.log("inside function nextshirt");
-    console.log(state.l);
-
     var shirt = document.getElementById("shirt");
 
-    if(state.l === 0){
-        shirt.src = ""
-        state.l++;
-        console.log(state.l);
+    if (state.l === 0) {
+        shirt.hidden = false
+        state.l = 1;
     }
-    else
-    if(state.l===1){
-        shirt.src = "shirt-1.png"
+    else if (state.l === 1) {
+        shirt.hidden = true
         state.l = 0;
-        console.log(state.l);
     }
 }
 
 function nextskin()
 {
-    console.log("inside function nextskin");
-    console.log(state.j);
-
     var skin = document.getElementById("skin");
-
-    if(state.j===0){
-        skin.src = "skin-1.png"
-        state.j++;
-        console.log(state.j);
-    }
-    else
-     if(state.j===1){
-        skin.src = "skin-2.png"
-        state.j++;
-        console.log(state.j);
-    }
-    if(state.j===2){
-        skin.src = "skin-3.png"
-        state.j++;
-        console.log(state.j);
-    }
-    if(state.j===3){
-        skin.src = "skin-4.png"
-        state.j++;
-        console.log(state.j);
-    }
-    else
-     if(state.j===4){
-        skin.src = "skin-5.png"
-         state.j=0;
-    }
-    
+    state.j = (state.j + 1) % skins.length
+    skin.src = skins[state.j];
 }
 
 function nexthair()
 {
-    console.log("inside function nexthair");
-    
-    console.log(state.k);
     var hairFront = document.getElementById("hairfront");
     var hairBack = document.getElementById("hairback");
-    
-    if(state.k===0){
-        hairFront.src = "hairfront-1.png"
-        hairBack.src = "hairback-1.png"
-        state.k++;
-        console.log(state.k);
-    }
-    else
-     if(state.k===1){
-        hairFront.src = "gingerhairfront.png"
-        hairBack.src = "gingerhairback.png"
-        state.k = 0;
-        console.log(state.k);
-    }
+
+    state.k = (state.k + 1) % hairs.length
+    hairFront.src = hairs[state.k][0];
+    hairBack.src = hairs[state.k][1];
 }
